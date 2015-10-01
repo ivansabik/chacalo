d90c391c4d81a3e5d5faa66677837d93e3255a5c
sabik <ivan.rodriguez.o@gmail.com>
2015-09-30 20:23:27 -0500

    moved to lib, tests definition, generating

---
+++ test/cli.js
@@ -0,0 +1,51 @@
+var assert = require('assert-diff');
+var exec = require('child_process').exec;
+
+it('download single song from youtube', function (done) {
+	var cmd = './chacalo';
+	exec(cmd, function(error, stdout, stderr) {
+		assert.fail('implement test');
+	});
+});
+
+it('download single song from soundcloud', function (done) {
+	var cmd = './chacalo';
+	exec(cmd, function(error, stdout, stderr) {
+		assert.fail('implement test');
+	});
+});
+
+it('download single song from mixcloud', function (done) {
+	var cmd = './chacalo';
+	exec(cmd, function(error, stdout, stderr) {
+		assert.fail('implement test');
+	});
+});
+
+it('download songs from youtube playlist', function (done) {
+	var cmd = './chacalo';
+	exec(cmd, function(error, stdout, stderr) {
+		assert.fail('implement test');
+	});
+});
+
+it('download songs from soundcloud playlist', function (done) {
+	var cmd = './chacalo';
+	exec(cmd, function(error, stdout, stderr) {
+		assert.fail('implement test');
+	});
+});
+
+it('download songs from mixcloud playlist', function (done) {
+	var cmd = './chacalo';
+	exec(cmd, function(error, stdout, stderr) {
+		assert.fail('implement test');
+	});
+});
+
+it('download songs from text file', function (done) {
+	var cmd = './chacalo';
+	exec(cmd, function(error, stdout, stderr) {
+		assert.fail('implement test');
+	});
+});
---
+++ test/unit.js
@@ -0,0 +1,31 @@
+var assert = require('assert-diff');
+
+it('choose youtube as provider based on the URL', function (done) {
+	assert.fail('implement test');
+});
+
+it('choose soundcloud as provider based on the URL', function (done) {
+	assert.fail('implement test');
+});
+
+it('choose mixcloud as provider based on the URL', function (done) {
+	assert.fail('implement test');
+});
+
+it('choose if mode is song or playlist based on the URL', function (done) {
+	assert.fail('implement test');
+});
+
+it('generate soundcloud playlist text file', function (done) {
+	assert.fail('implement test');
+
+});
+
+it('generate youtube playlist text file', function (done) {
+	assert.fail('implement test');
+
+});
+
+it('generate mixcloud playlist text file', function (done) {
+	assert.fail('implement test');
+});
